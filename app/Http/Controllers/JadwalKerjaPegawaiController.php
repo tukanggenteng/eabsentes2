3c26f7508aca90a03fa70cfd55904fc1e04cd699
Diskominfo <diskominfo@MacBook-Pro.local>
2017-12-11 22:59:02 +0800

    asd

--- app/Http/Controllers/JadwalKerjaPegawaiController.php
+++ app/Http/Controllers/JadwalKerjaPegawaiController.php
@@ -64,7 +64,7 @@ class JadwalKerjaPegawaiController extends Controller
                 //->where('tanggal_akhirrule','>=',$tanggalsekarang)
                 ->where('jadwalkerjas.jenis_jadwal','like','%'.$request->table_search2.'%')
                 ->orWhere('pegawais.nip','like','%'.$request->table_search2.'%')
-                ->where('pegawais.instansi_id','=',Auth::user()->instansi_id)
+                ->orWhere('pegawais.instansi_id','=',Auth::user()->instansi_id)
                 // ->orWhere('pegawais.nama','like','%'.$request->table_search2.'%')
                 // ->orWhere('rulejadwalpegawais.tanggal_awalrule','like','%'.$request->table_search2.'%')
                 // ->where('rulejadwalpegawais.tanggal_akhirrule','like','%'.$request->table_search2.'%')
@@ -104,7 +104,7 @@ class JadwalKerjaPegawaiController extends Controller
                 ->select('rulejadwalpegawais.id','pegawais.nip','pegawais.nama','jadwalkerjas.jenis_jadwal','rulejadwalpegawais.tanggal_awalrule','rulejadwalpegawais.tanggal_akhirrule')
                 ->orderBy('rulejadwalpegawais.id','ASC')
                 ->paginate(30);
-           dd($rulejadwal2);
+        //    dd($rulejadwal2);
             $rulejadwal=pegawai::where('instansi_id',Auth::user()->instansi_id)->paginate(30);
             $jadwalkerja=jadwalkerja::all();
 //            dd($rulejadwal);
