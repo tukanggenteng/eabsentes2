c57cf2f02cda63a64a6deb28e6bfc063875599aa
Diskominfo <diskominfo@MacBook-Pro.local>
2017-12-14 07:26:01 +0800

    admin

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -472,8 +472,8 @@ class PegawaiController extends Controller
     }
 
     public function getadminparams($id){
-      $table=pegawai::where('id','=','7239')
-                ->orWhere('id','=','10219')
+      $table=pegawai::where('id','=','10219')
+                // ->orWhere('id','=','7239')
                 ->where('id','=',$id)
                 ->get();
       return $table;
