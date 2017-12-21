9e58a783157d0ce1098df092f12cf959e8b809d6
Diskominfo <diskominfo@MacBook-Pro.local>
2017-12-21 08:55:02 +0800

    class

--- resources/views/errors/504.blade.php
+++ resources/views/errors/504.blade.php
@@ -1,6 +1,6 @@
 <style>
 #fof{display:block; position:relative; width:100%; margin:150px 0; text-align:center;}
-#fof .positioned{display:block; width:85%; margin:0 auto; padding:20px; border:1px solid #CCCCCC;}
+#fof .positioned{font-family: Arial, Helvetica, sans-serif;display:block; width:85%; margin:0 auto; padding:20px; border:1px solid #CCCCCC;}
 #fof .positioned h1{margin:0 0 0 20px; padding:0; display:inline; font-size:60px; text-transform:uppercase;}
 #fof .positioned p{margin:25px 0 0 0; padding:0; font-size:16px;}
 #fof a.go-back, #fof a.go-home{display:block; position:absolute; top:30px; width:100px; padding:20px 0; font-size:20px; text-transform:uppercase; color:#FFFFFF; background-color:#FF6600;}
