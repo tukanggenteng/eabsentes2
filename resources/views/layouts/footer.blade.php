9eefbd2677561cb4b6887fe0fbdc9c25ed0cbe03
Diskominfo <diskominfo@MacBook-Pro.local>
2018-07-03 13:16:40 +0800

    version

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.4.a
+        <b>Version</b> 2.4.b
     </div>
     <strong>Developped by <a href="http://diskominfo.kalselprov.go.id">Diskominfo Prov. Kalsel</a>.</strong>  &copy; 2017 
 </footer>
