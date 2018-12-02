6e92ad18cf8a6075a5983eaf51572409cb07e496
Diskominfo <diskominfo@MacBook-Pro.local>
2018-12-02 15:55:08 +0800

    tes

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -2625,7 +2625,7 @@ class MonitoringController extends Controller
                                     // ->whereNotNull('atts.jam_masuk')
                                     ->where('atts.tanggal_att','!=',$tanggalexception)
                                     ->select(
-                                        DB::raw('count(if (atts.jenisabsen_id < jadwalkerjas.jam_keluarjadwal && atts.jam_masuk is not null && jam_keluar is null,1,0)) as data')
+                                        DB::raw('ROUND(count(if (atts.jenisabsen_id < jadwalkerjas.jam_keluarjadwal && atts.jam_masuk is not null && jam_keluar is null,1,0) / count(if(atts.jenisabsen_id!="9" && atts.jenisabsen_id != "11" && atts.jenisabsen_id!="13",1,0)) * 100 ),2) as data')
 
                                     )
                                     // ->groupBy(DB::raw('EXTRACT(YEAR_MONTH FROM atts.tanggal_att)'),DB::raw('pegawais.instansi_id'))                
