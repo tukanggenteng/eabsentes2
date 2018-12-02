ee3e13e758aaec2e80f85a5315d4acddeed13dff
Diskominfo <diskominfo@MacBook-Pro.local>
2018-12-02 19:47:02 +0800

    tes

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -2760,7 +2760,7 @@ class MonitoringController extends Controller
                                 DB::raw('ROUND((((count(if (atts.jenisabsen_id = "1" && atts.jam_keluar is not null,1,null))) + (count(if (atts.jenisabsen_id = "3",1,null))) + (count(if (atts.jenisabsen_id = "5",1,null))) + (count(if (atts.jenisabsen_id = "4",1,null))) + (count(if (atts.jenisabsen_id = "7",1,null))) + (count(if (atts.jenisabsen_id = "6",1,null))) + (count(if (atts.jenisabsen_id = "8",1,null))) + (count(if (atts.jenisabsen_id = "10",1,null))) + (count(if (atts.jenisabsen_id = "12",1,null)))) / (count(if(atts.jenisabsen_id!="9" && atts.jenisabsen_id != "11" && atts.jenisabsen_id!="13",1,null))) * 100),2 ) as persentase_kehadiran'),
                                 'instansis.namaInstansi',
                                 DB::raw('ROUND(count(if (atts.jenisabsen_id = "1" && atts.jam_keluar is not null,1,null)) / count(if(atts.jenisabsen_id!="9" && atts.jenisabsen_id != "11" && atts.jenisabsen_id!="13",1,null)) * 100
-                                ,2) as data'),
+                                ,2) as data')
                         )
                         ->groupBy(DB::raw('EXTRACT(YEAR_MONTH FROM atts.tanggal_att)'),DB::raw('pegawais.instansi_id'))                
                         ->whereMonth('atts.tanggal_att','=',$bulan)
