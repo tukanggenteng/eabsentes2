3a689e40efe0091243bc3f50ede5edcd1b98cd72
Diskominfo <diskominfo@MacBook-Pro.local>
2019-01-09 18:27:55 +0800

    tes

--- resources/views/jadwalkerja/jadwalkerjadetail.blade.php
+++ resources/views/jadwalkerja/jadwalkerjadetail.blade.php
@@ -100,7 +100,7 @@
 
                 <div class="box box-default">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Atur Jam Kerja</h3>
+                        <h3 class="box-title">Jadwal Kerja</h3>
 
                         <div class="box-tools pull-right">
                             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
@@ -320,7 +320,7 @@
         });
         // var currColor = '{{$jadwalkerja->color}}' //Red by default
         // //Color chooser button
-        // $('#preview')[0].className = '{{$jadwalkerja->classdata}}';
+        $('#preview')[0].className = '{{$jadwalkerja->classdata}}';
         // var colorChooser = $('#color-chooser-btn')
         // $('#color-chooser > li > a').click(function (e) {
         //     e.preventDefault()
