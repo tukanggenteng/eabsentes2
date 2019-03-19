d77c17f7b334b187782f6ddf4c2ea9a19c72f501
Diskominfo <diskominfo@MacBook-Pro.local>
2019-03-20 00:52:36 +0800

    new queue

--- app/Http/Controllers/QueuePegawaiController.php
+++ app/Http/Controllers/QueuePegawaiController.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 
 class QueuePegawaiController extends Controller
 {
-    
+    // add column dengan ip nya
     public function index(Request $request)
     {
       
@@ -24,19 +24,46 @@ class QueuePegawaiController extends Controller
 
         $instansi_id=$request->json('instansi');
         $macaddress=$request->json('macaddress');
-
+        $fingerprint_ip=$request->json('fingerprint_ip');
+        $queue_id=$request->json('id');
         
         $datainstansi=instansi::where('id','=',$instansi_id)
                       ->first();
         $datamacaddress=macaddresse::where('macaddress','=',$macaddress)
                       ->first();
 
+        if ($datamacaddress->instansi_id==null)
+        {
+            $datamacaddress->instansi_id=$instansi_id;
+            $datamacaddress->save();
+        }
+
+                     
+
         $dataqueuepegawai= queue_pegawai::where('instansi_id','=',$instansi_id)
-        ->where('macaddress_id','=',$datamacaddress->id)
-        ->where('status','=',false)
-        ->get();
+                        ->where('macaddress_id','=',$datamacaddress->id)
+                        ->where('fingerprint_ip','=',$fingerprint_ip);
+
+
+        if ($dataqueuepegawai->count()==0)
+        {
+            $datapegawais=pegawai::where('instansi_id','=',$instansi_id)->get();
+
+            foreach ($datapegawais as $key => $datapegawai)
+            {
+
+                $addqueuepegawai= new queue_pegawai();
+                $addqueuepegawai->pegawai_id=$datapegawai->id;
+                $addqueuepegawai->macaddress_id=$datamacaddress->id;
+                $addqueuepegawai->instansi_id=$instansi_id;
+                $addqueuepegawai->fingerprint_ip=$fingerprint_ip;
+                $addqueuepegawai->command="daftar";
+                $addqueuepegawai->status=false;
+                $addqueuepegawai->save();
+            }
+        }
         
-        return $dataqueuepegawai;
+        return $dataqueuepegawai->where('status','=',false)->get();
 
     }
 
@@ -45,19 +72,16 @@ class QueuePegawaiController extends Controller
 
         $instansi_id=$request->json('instansi');
         $macaddress=$request->json('macaddress');
+        $fingerprint_ip=$request->json('fingerprint_ip');
+        $queue_id=$request->json('id'); 
         $token=$request->json('token');
 
-        $hasilbasic=$macaddress.$instansi_id.$pegawai_id;
+        $hasilbasic=$macaddress.$instansi_id.$pegawai_id.$fingerprint_ip.$queue_id;
         $hasil=$this->encryptOTP($hasilbasic); 
 
-       
-        $datainstansi=instansi::where('id','=',$instansi_id)
-                      ->first();
-        $datamacaddress=macaddresse::where('macaddress','=',$macaddress)
-                      ->first();
 
         $dataqueuepegawai= queue_pegawai::where('instansi_id','=',$instansi_id)
-        ->where('macaddress_id','=',$datamacaddress->id)
+        ->where('id','=',$queue_id)
         ->first();
 
         $dataqueuepegawai->status=true;
